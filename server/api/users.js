682e0f891b814489b0448e5e10b21ed7f4cadc55
Stephen Chow <stephenchow2k@gmail.com>
2018-08-03 18:06:05 -0400

    push fix

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../TEST/TEST');
+const { User } = require('../db/models');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
